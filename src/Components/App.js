366bbc5a56c72f0c9690373a8302dde93a701d3c
William Rodriguez <william@rodriguez1.com>
2022-07-01 16:12:06 -0700

    Update App component with TapControl

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TapControl from "./TapControl";
+
+
+function App() {
+  return (
+    <React.Fragment>
+			<h1>Tap Tracker</h1>
+			<TapControl />
+		</React.Fragment>
+  );
+}
+
+export default App;
